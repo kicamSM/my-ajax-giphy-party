a829a0a8436fd9812718d54f52811c8688f0f8ef
kicamSM <kicamSMM@hotmail.com>
2023-02-12 11:59:27 -0700

    got index variable working

--- myApp.js
+++ myApp.js
@@ -1,30 +1,15 @@
-console.log("Let's get this party started!");
-
 async function getGiphy(searchTerm) {
     const response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
 
-    console.log(response.data);
-    const img = document.getElementById('giphy');
     const maxCnt = response.data['data'].length; 
-    const rndmNum = Math.floor(Math.random * maxCnt);
+    const rndmNum = Math.floor(Math.random() * maxCnt);
     const createdImg = document.createElement('img');
+    const imgDiv = document.getElementById('image-div');
+    
     createdImg.setAttribute('src', "");
     createdImg.setAttribute('class', 'appendedImg');
-    const imgDiv = document.getElementById('image-div');
     imgDiv.append(createdImg);
-    createdImg.src = response.data['data'][32]['images']['original']['url'];
-        //nothing outside of 50 works for cat 
-    // const img = response.data.images.url
-    // for(let url of images)
-    // const newLi = document.createElement('li'); 
-    // newLI.innerText = // put the url here 
-    // ul.append(newLI); //note you need a ul in your html page 322 in notes 
-//  console.log(response.data['data'][0]['images']['original']['url']);
-//  console.log(response.data['data'][rndmNum]['images']['original']['url']);
-// console.log(response.data['pagination']['total_count'])
-console.log(Math.floor(Math.random() * (maxCnt)))
-console.log(response.data['data'].length)
-// console.log(response.data['data'][rndmNum]['images']['original']['url'])
+    createdImg.src = response.data['data'][rndmNum]['images']['original']['url'];
 }
 
 const input = document.getElementById('input')
